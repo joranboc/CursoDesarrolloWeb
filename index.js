4b16e82288c8cefdd955e6339bd7f93ac6afee23
joranboc <joranboc@hotmail.com>
2019-06-11 19:54:02 -0500

    Segunda clase

---
+++ index.js
@@ -0,0 +1,40 @@
+
+var numero1 = 5
+var numero2 = 6
+var suma = numero1 + numero2
+var restar = numero1 - numero2
+var multiplicar = numero1 * numero2
+var divir = numero1 / numero2
+//alert(suma)
+console.log("La suma de "+ numero1 +" mas "+numero2+ " es igual " + suma)
+console.log(`La suma de ${numero1} mas ${numero2} es igual `+suma)
+console.log(restar)
+console.log(multiplicar)
+console.log(divir)
+
+var texto1= "Esto es un 'texto'"
+var texto2= 'Esto es un "texto"'
+var text3= `Esto es un texto`
+var diasSemana = ["lunes","martes",
+"miercoles","jueves","viernes"]
+
+var bool = true
+
+var dia = "martes"
+/*if (dia ==="viernes"){
+    alert("Nos vamos de rumba")
+}else{
+    alert("No podemos irnos de rumba :( porque es "+ dia )
+}*/
+
+for(var index=0;index < diasSemana.length;index++){
+    alert(diasSemana[index])
+}
+function jorge() {
+    var nombre = jorge;
+}
+let nombre;
+/*for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+}*/
